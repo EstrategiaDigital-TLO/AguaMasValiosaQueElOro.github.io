9216e597e700fcd7b16a46fbdf4e35e24ded3ad8
EstrategiaDigital-TLO <olivovazquezjosealejandro@gmail.com>
2024-11-29 18:09:34 -0600

    Add files via upload

---
+++ javascript/sliderscript.js
@@ -0,0 +1,19 @@
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let index = 0;
+function nextSlide() {
+    index++;
+    if (index > images.length - 1) {
+        index = 0;
+    }
+    slider.style.marginLeft = -index * 100 + '%';
+}
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = images.length - 1;
+    }
+    slider.style.marginLeft = -index * 100 + '%';
+}
